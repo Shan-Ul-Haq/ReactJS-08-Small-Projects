b2c908bbdb538d9ab0f0e718fa0ff8e8cac9d310
Shan-Ul-Haq <shanulhaq84@gmail.com>
2024-01-29 12:19:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReactJS-08-Small-Projects
+ Build 08 React JS Projects which included Accordian, Random Color, Star Rating, Image Slider, Load more data, Tree view, QR Code Generator & Ligh and Dark mode.
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data';
+import QRCodeGenerator from './components/qr-code-generator';
+import LightDarkMode from './components/light-dark-mode';
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian/>
+      <RandomColor/>
+      <StarRating noOfStars={10}/>
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"25"}/>
+      <LoadMoreData />
+      <TreeView menus={menus}/>
+      <QRCodeGenerator/>
+      <LightDarkMode/>
+      
+    </div>
+  );
+}
+
+export default App;
